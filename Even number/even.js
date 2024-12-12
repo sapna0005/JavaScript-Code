a2378486d64d8361aa84df8a28e2471d961b8b74
sapna0005 <sapna.sahani0005@gmail.com>
2024-12-12 23:22:08 +0530

    find even & odd number

---
+++ Even number/even.js
@@ -0,0 +1,11 @@
+function start(){
+
+let number=prompt("Enter your number")
+    if (number%2==0) {
+        alert("number is even")
+    }
+    else{
+        alert("number is odd")
+    }
+}
+start()
