effa33ce9e368ee12f8832e7ab24d4bee6375b40
sapna0005 <sapna.sahani0005@gmail.com>
2024-12-18 23:51:51 +0530

    added arrow code

---
+++ arrow function/arrow.js
@@ -0,0 +1,16 @@
+// let start=()=>{
+//     alert("Button Clicked")
+
+
+// }
+
+let start=()=>{
+    // document.getElementById("text")
+    // document.getElementsByClassName("text")
+    let show=document.querySelector("#text")
+    show.innerHTML="<h1>This is HTML </h1>"
+    show.style.color="red";
+    show.style.fontSize="50px";
+    
+
+}
