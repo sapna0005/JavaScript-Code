b2279b3159dafcb3ec158343909a8513518c531b
sapna0005 <sapna.sahani0005@gmail.com>
2024-12-22 22:12:44 +0530

    added task

---
+++ Task/task.js
@@ -0,0 +1,16 @@
+
+//   Question
+//   Confirm to Show Message
+//   Ask the user: "Do you like JavaScript?" using confirm.
+//   If they click OK, show an alert with "That's great!".
+//   If they click Cancel, show an alert with "No worries, keep learning!".
+
+   
+//Answer  
+   let a=confirm("Do you like JavaScript")
+    if (a==true) {
+        alert("That's great!")
+    }
+    else{
+        alert("No worries, keep learning!")
+    }
