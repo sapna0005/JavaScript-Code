c6d82d5d5fa312f5670c0a28d63e10928cdc9fad
sapna0005 <sapna.sahani0005@gmail.com>
2024-12-27 14:24:45 +0530

    added form validation task

--- Form Validation/form.js
+++ Form Validation/form.js
@@ -20,28 +20,61 @@ let submitt=()=>{
         return false;
     }
 
-    if (mobile==="") {
-        errormobile.innerHTML="Please enter your mobile no"
-        errormobile.style.color="red"
-        return false;
-    }
+    // if (mobile==="") {
+    //     errormobile.innerHTML="Please enter your mobile no"
+    //     errormobile.style.color="red"
+    //     return false;
+    // }
 
-    if (email==="") {
-        erroremail.innerHTML="Please enter your email"
-        erroremail.style.color="red"
-        return false;
-    }
+    // if (email==="") {
+    //     erroremail.innerHTML="Please enter your email"
+    //     erroremail.style.color="red"
+    //     return false;
+    // }
 
-    if (password==="") {
-        errorpassword.innerHTML="Please enter your password"
-        errorpassword.style.color="red"
-        return false;
-    }
+    // if (password==="") {
+    //     errorpassword.innerHTML="Please enter your password"
+    //     errorpassword.style.color="red"
+    //     return false;
+    // }
   
-    if (confirmpassword==="") {
-        errorconfirmpassword.innerHTML="Please confirm your password"
-        errorconfirmpassword.style.color="red"
+    // if (confirmpassword==="") {
+    //     errorconfirmpassword.innerHTML="Please confirm your password"
+    //     errorconfirmpassword.style.color="red"
+    //     return false;
+    // }
+
+    else if(isNaN(mobile)){
+        errormobile.innerHTML="Please enter a valid number";
         return false;
-    }
+      }
+
+      else if(mobile.length!=10){
+        errormobile.innerHTML="Please enter a 10 digit number";
+        return false;
+      }
+
+      else if(!(email.includes('@') && email.includes('.com'))){
+        erroremail.innerHTML="Please enter a valid email";
+        return false;
+      }
+
+
+      else if(password!=confirmpassword){
+        errorconfirmpassword.innerHTML="Password and confirm password should be same";
+        return false;
+      }
+
+
+      else if(!(
+        password.match(/[1234567890]/) &&
+        password.match(/[!@#$%^&*()]/) &&
+        password.match(/[a-z]/) &&
+        password.match(/[A-Z]/)
+      ))
+      {
+        errorpassword.innerHTML="Password should have at least one uppercase letter, one lowercase letter, one digit"
+         return false;
+      }
 
 }
