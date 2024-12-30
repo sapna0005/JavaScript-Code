d254d8d8dffadc9dda37c2588d7a0655730bff03
sapna0005 <sapna.sahani0005@gmail.com>
2024-12-30 23:59:50 +0530

    added code

--- object/object.js
+++ object/object.js
@@ -6,3 +6,18 @@ let student={
 console.log(student); 
 console.log(student.age); 
 
+// Spread operator
+
+const student1=
+{
+    name:"John", 
+    age:25, 
+}
+const student2=
+{
+    city:"New York",
+    Grade:'A'
+}
+const combinedstudent={...student1, ...student2}
+console.log(combinedstudent);
+
