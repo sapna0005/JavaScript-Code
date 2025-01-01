7766cd1ca12149bbb2991804aabdf6c83b63ffb7
sapna0005 <sapna.sahani0005@gmail.com>
2025-01-01 22:19:14 +0530

    added SetInterval

--- SetInterval/setinterval.js
+++ SetInterval/setinterval.js
@@ -1,6 +1,14 @@
-let startt=()=>{
-      setInterval(()=>{
-        console.log("running");
+// let startt=()=>{
+//       setInterval(()=>{
+//         console.log("running");
         
+//       },2000)
+//     }
+
+    let startt=()=>{
+      var counter=0;
+      var inter= setInterval(()=>{
+      console.log(counter);
+      counter++;
       },2000)
     }
