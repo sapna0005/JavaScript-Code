ef8f565b45a46c5e8924fc4c1541a6f345cbeee9
sapna0005 <sapna.sahani0005@gmail.com>
2025-01-13 23:35:59 +0530

    added setTimeout

---
+++ SetTimeout/settimeout.js
@@ -0,0 +1,12 @@
+setTimeout(function() {
+      document.getElementById('display').innerText = 'welcome';
+    }, 2000); 
+    
+    
+    setTimeout(function() {
+      document.getElementById('display').innerText = 'goodbye';
+    }, 4000);
+    
+    setTimeout(function() {
+      document.getElementById('display').remove();
+    }, 6000);
