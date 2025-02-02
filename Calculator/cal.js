096a3e3c75129099c4b6d9043b1c7c9e21c1f3bb
sapna0005 <sapna.sahani0005@gmail.com>
2025-02-02 22:53:30 +0530

    added

--- Calculator/cal.js
+++ Calculator/cal.js
@@ -13,6 +13,17 @@ let equall=()=>{
     store=eval(store) // 3 number
     store.toString();  //"3" string
 }
+let presses = (value) => {
+    let show = document.querySelector("#display");
+    
+    if (value === 'Math.PI') {
+        store += Math.PI.toFixed(5); // Append π (3.14159) with 5 decimal places
+    } else {
+        store += value; // Append the input value as usual
+    }
+    
+    show.innerHTML = store;
+}
 
 let del=()=>{
     let show=document.querySelector("#display")
@@ -29,6 +40,11 @@ let clearr=()=>{
 let sql=()=>{
     let show=document.querySelector("#display")
     store=store*store;
+    show.innerHTML=store;
+}
+let cube=()=>{
+    let show=document.querySelector("#display")
+    store=store*store*store;
     show.innerHTML=store;
 }
 
